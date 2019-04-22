03ebb9eaf8d6cd662d3735f577a0c8ca3735b93d
David Holtz <david.richard.holtz@gmail.com>
2019-04-22 10:24:27 -0400

    make params public and exportable

--- go-ethereum/zkproofs/bb.go
+++ go-ethereum/zkproofs/bb.go
@@ -24,31 +24,31 @@ Eurocrypt 2004
 */
 
 import (
-	"errors"
 	"crypto/rand"
-	"math/big"
+	"errors"
 	"github.com/ing-bank/zkproofs/go-ethereum/crypto/bn256"
+	"math/big"
 )
 
 type keypair struct {
-	pubk *bn256.G1
-	privk *big.Int
+	Pubk  *bn256.G1
+	Privk *big.Int
 }
 
 /*
-keygen is responsible for the key generation. 
+keygen is responsible for the key generation.
 */
 func keygen() (keypair, error) {
 	var (
-		kp keypair
-		e error
+		kp  keypair
+		e   error
 		res bool
 	)
-	kp.privk, e = rand.Int(rand.Reader, bn256.Order)
+	kp.Privk, e = rand.Int(rand.Reader, bn256.Order)
 	if e != nil {
 		return kp, e
 	}
-	kp.pubk, res = new(bn256.G1).Unmarshal(new(bn256.G1).ScalarBaseMult(kp.privk).Marshal())
+	kp.Pubk, res = new(bn256.G1).Unmarshal(new(bn256.G1).ScalarBaseMult(kp.Privk).Marshal())
 	if res {
 		return kp, errors.New("Could not compute scalar multiplication.")
 	}
@@ -56,14 +56,14 @@ func keygen() (keypair, error) {
 }
 
 /*
-sign receives as input a message and a private key and outputs a digital signature. 
+sign receives as input a message and a private key and outputs a digital signature.
 */
-func sign(m *big.Int, privk *big.Int) (*bn256.G2, error) {
+func sign(m *big.Int, Privk *big.Int) (*bn256.G2, error) {
 	var (
-		res bool
+		res       bool
 		signature *bn256.G2
 	)
-	inv := ModInverse(Mod(Add(m, privk),bn256.Order), bn256.Order)
+	inv := ModInverse(Mod(Add(m, Privk), bn256.Order), bn256.Order)
 	signature, res = new(bn256.G2).Unmarshal(new(bn256.G2).ScalarBaseMult(inv).Marshal())
 	if res != false {
 		return signature, nil
@@ -74,20 +74,20 @@ func sign(m *big.Int, privk *big.Int) (*bn256.G2, error) {
 
 /*
 verify receives as input the digital signature, the message and the public key. It outputs
-true if and only if the signature is valid. 
+true if and only if the signature is valid.
 */
-func verify(signature *bn256.G2, m *big.Int, pubk *bn256.G1) (bool, error) {
+func verify(signature *bn256.G2, m *big.Int, Pubk *bn256.G1) (bool, error) {
 	// e(y.g^m, sig) = e(g1,g2)
 	var (
-		gm *bn256.G1
+		gm     *bn256.G1
 		e, res bool
 	)
-	// g^m 
+	// g^m
 	gm, e = new(bn256.G1).Unmarshal(new(bn256.G1).ScalarBaseMult(m).Marshal())
 	// y.g^m
-	gm = gm.Add(gm, pubk)
+	gm = gm.Add(gm, Pubk)
 	// e(y.g^m, sig)
-	p1 := bn256.Pair(gm, signature)	
+	p1 := bn256.Pair(gm, signature)
 	// e(g1,g2)
 	g1 := new(bn256.G1).ScalarBaseMult(new(big.Int).SetInt64(1))
 	g2 := new(bn256.G2).ScalarBaseMult(new(big.Int).SetInt64(1))
@@ -98,6 +98,6 @@ func verify(signature *bn256.G2, m *big.Int, pubk *bn256.G1) (bool, error) {
 	res = p1.IsOne()
 	if e != false {
 		return res, nil
-	} 
+	}
 	return false, errors.New("Error while computing signature.")
 }
